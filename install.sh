addf015965f30f311021ed78e24b5a5b24f6bac1
Pat Network <iampatmr@hotmail.com>
2016-03-12 13:56:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,4 +22,7 @@ echo "##                                                          ##"
 echo "##############################################################"
 
 apt-get install -y
-apt-get install 
+apt-get install build-essential libssl-dev php5-gd freeradius freeradius-mysql freeradius-utils libapache2-mod-auth-mysql ssl-cert php5-curl php5-cli iptables php5 php5-common php5-gd php-pear php-db libapache2-mod-php5 php5-mysql phpmyadmin
+
+echo "ServerName localhost" | sudo tee /etc/apache2/conf-available/fqdn.conf
+ sudo a2enconf fqdn
